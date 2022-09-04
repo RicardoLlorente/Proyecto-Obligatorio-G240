1851c9aef2fa16245392c5deb96c2f26da7440b4
RicardoLlorente <ricardo.llorente.95@gmail.com>
2022-09-04 03:10:09 -0300

    Entrega2 completo con desafiate

--- js/products.js
+++ js/products.js
@@ -1,58 +1,73 @@
-//array donde se cargarán los datos recibidos con los productos de la categoria deseada:
-let ProductosCategoria = [];
-const ORDER_ASC_BY_COST = ">";
-const ORDER_DESC_BY_COST = "<";
-const ORDER_BY_PROD_SOLDCOUNT = "Vendidos";
-let CriterioActual = undefined;
-let mincost = undefined;
-let maxcost = undefined;
+                ////////////////////////////////////
+                /////V̳a̳r̳i̳a̳b̳l̳e̳s̳ y̳ C̳o̳n̳s̳t̳a̳n̳t̳e̳s̳ ////////
+                /////////u̳t̳i̳l̳i̳z̳a̳d̳a̳s̳/////////////////
+                ////////////////////////////////////
+
+let ListadeProductos = [];//array donde se cargarán los datos recibidos con los productos de la categoria deseada:
+
+////Constantes para los criterios de orden en los filtros
+const ORDENAR_ASC_POR_COSTO = ">";
+const ORDENAR_DESC_POR_COSTO= "<";
+const ORDER_POR_CANT_VEND = "Vendidos";
+
+///variables para mostrar productos solo en un determinado rango de precios 
+let CostoMinimo = undefined;
+let CostoMaximo = undefined;
 
 
-function OrdenaProductos(criterio, Objeto) {
-    let result = [];
-    if (criterio === ORDER_ASC_BY_COST) {
-        result = Objeto.products.sort(function (a, b) {
-            let acost = parseInt(a.cost);
-            let bcost = parseInt(b.cost);
-            if (acost < bcost) { return -1; }
-            if (acost > bcost) { return 1; }
+    ///////////////////////////////////////////////////////////////////
+    ///////////B̳l̳o̳q̳u̳e̳ d̳e̳ f̳u̳n̳c̳i̳o̳n̳e̳s̳ u̳t̳i̳l̳i̳z̳a̳d̳a̳s̳ e̳n̳ l̳o̳s̳ e̳v̳e̳n̳t̳o̳s̳///////////
+    ///////////////////////////////////////////////////////////////////
+
+
+//Esta funcion recibe un objeto (ListaProd) y un criterio para ordenar 
+//los productos del campo products de dicho Objeto
+
+function OrdenaProductos(CriterioDeOrden, ListaProd) {
+   
+    let ProductosOrdenados = [];// Arreglo que tendra los productos luego de haberlos ordenados. 
+    
+    //En cada caso, dependiendo del criterio, ordeno los elementos del campo 
+    // products del Objeto (usando la funcion sort) y luego los dejo almacenados en 
+    // el arreglo llamado ProductosOrdenados.
+    if (CriterioDeOrden === ORDENAR_ASC_POR_COSTO) {
+        ProductosOrdenados = ListaProd.products.sort(function (a, b) {
+            let costo1 = parseInt(a.cost);
+            let costo2 = parseInt(b.cost);
+            if (costo1 < costo2) { return -1; }
+            if (costo1 > costo2) { return 1; }
             return 0;
         });
-    } else if (criterio === ORDER_DESC_BY_COST) {
-        result = array.products.sort(function (a, b) {
-            let acost = parseInt(a.cost);
-            let bcost = parseInt(b.cost);
-            if (acost > bcost) { return -1; }
-            if (acost < bcost) { return 1; }
+    } else if (CriterioDeOrden === ORDENAR_DESC_POR_COSTO) {
+        ProductosOrdenados = ListaProd.products.sort(function (a, b) {
+            let costo1 = parseInt(a.cost);
+            let costo2 = parseInt(b.cost);
+            if (costo1 > costo2) { return -1; }
+            if (costo1 < costo2) { return 1; }
             return 0;
         });
-    } else if (criterio === ORDER_BY_PROD_SOLDCOUNT) {
-        result = array.products.sort(function (a, b) {
-            let asoldCount = parseInt(a.soldCount);
-            let bsoldCount = parseInt(b.soldCount);
-            if (asoldCount > bsoldCount) { return -1; }
-            if (asoldCount < bsoldCount) { return 1; }
+    } else if (CriterioDeOrden === ORDER_POR_CANT_VEND) {
+        ProductosOrdenados = ListaProd.products.sort(function (a, b) {
+            let cantvend1 = parseInt(a.soldCount);
+            let cantvend2 = parseInt(b.soldCount);
+            if (cantvend1 > cantvend2) { return -1; }
+            if (cantvend1 < cantvend2) { return 1; }
             return 0;
         });
     }
-
-    return result;
+    //Le asigno el arreglo ProductosOrdenados, que ya tendra los productos 
+    //ordenados con el orden deseado, al campo products de la variable global ListadeProductos.
+    ListadeProductos.products=ProductosOrdenados;
 }
-
-
-
-
-//función que recibe un arreglo con los productos, y los muestra en pantalla usando un DOM
-function MostrarCategoria(Categoria) {
+//función que recibe un arreglo con los productos, y los muestra en pantalla usando metodo innerHTML
+function MostrarListaProductos(ListaProd) {
     let htmlContentToAppend = "";
-    let Nombre = Categoria.catName;
-    alert(Nombre)
-    document.getElementById("parrafo").innerHTML += Categoria.catName + '.';
 
-    for (let i = 0; i < Categoria.products.length; i++) {
-        let Productos = Categoria.products[i];
-        if (((mincost == undefined) || (mincost != undefined && parseInt(Productos.cost) >= mincost)) &&
-            ((maxcost == undefined) || (maxcost != undefined && parseInt(Productos.cost) <= maxcost))) {
+    for (let i = 0; i < ListaProd.products.length; i++) {
+        let Productos = ListaProd.products[i];
+
+        if (((CostoMinimo == undefined) || (CostoMinimo != undefined && parseInt(Productos.cost) >= CostoMinimo)) &&
+            ((CostoMaximo == undefined) || (CostoMaximo != undefined && parseInt(Productos.cost) <= CostoMaximo))) {
             htmlContentToAppend += `
         <div class="list-group-item list-group-item-action prod">
             <div class="row ">
@@ -75,105 +90,101 @@ function MostrarCategoria(Categoria) {
         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
     }
 }
-/* https://github.com/angimenez/EjerciciosSort
-from Capacitador_JaP_1 to everyone:    10:22 AM
-https://github.com/angimenez/EjerciciosFilter */
-function OrdenaYMuestra(CriterioDeOrden, productos) {
-    CriterioActual = CriterioDeOrden;
-    if (productos != undefined) {
-        ProductosCategoria = productos;
-    }
-    ProductosCategoria.products = OrdenaProductos(CriterioActual, ProductosCategoria);
-    //Muestro las categorías ordenadas
-    MostrarCategoria(ProductosCategoria);
-}
+//Funcion que se usa para filtrar en tiempo real un producto buscado
 function BuscaYMuestra(producto) {
-    let filter, productos, nombre,desc, i;
-    filter = producto.toUpperCase();
-    productos = document.getElementsByClassName("prod");
-    for (i = 0; i < productos.length; i++) {
-        nombre = productos[i].getElementsByTagName("h4")[0];
-        desc = productos[i].getElementsByClassName("desc")[0];
-        console.log(nombre);
-        if (nombre) {
-            if ((nombre.innerHTML.toUpperCase().indexOf(filter) > -1)||(desc.innerHTML.toUpperCase().indexOf(filter) > -1)) {
-                productos[i].style.display = "";
-            } else {
-                productos[i].style.display = "none";
-            }
+    let nombre,desc;
+    let filter = producto.toUpperCase();//Elemento que se quiere buscar pasado a mayusculas
+    let productos = document.getElementsByClassName("prod");
+    for (let articulo of productos) {
+        nombre = articulo.getElementsByTagName("h4")[0];//Nombre de cada elemento de la lista de productos
+        desc = articulo.getElementsByClassName("desc")[0];//Descripcion de cada elemento en la lista de productos
+        
+        //En el caso de que el producto buscado coincida en alguna letra con alguna de las letras del nombre 
+        //o de la descricion de alguno de los productos lo sigo mostrando
+        
+        if ((nombre.innerHTML.toUpperCase().indexOf(filter) > -1)||(desc.innerHTML.toUpperCase().indexOf(filter) > -1)) {
+            articulo.style.display = "";  
+        } else {//En el caso de que no coincida dejo de mostrar dicho elemento
+            articulo.style.display = "none";
         }
-
     }
 }
 
-
-
+        ////////////////////////////////////////////////////
+        ///////////B̳l̳o̳q̳u̳e̳ d̳e̳ e̳v̳e̳n̳t̳o̳s̳ d̳e̳ l̳a̳ p̳a̳g̳i̳n̳a̳///////////
+        ////////////////////////////////////////////////////
 /* 
 EJECUCIÓN:
 -Al cargar la pagina se llama a la funcion getJSONData pasandole como parametro la direccion de los productos 
 con el respectivo valor de catID que va a indicar si se quiere leer la seccion de autos, juguetes o muebles.
-
--Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en CategoriaProductos.
-
--Por último, se llama a la funcion MostrarCategoria() pasándole por parámetro el arreglo CategoriaProductos.
-
+-Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en ListaProdProductos.
+-Por último, se llama a la funcion MostrarListaProductos() pasándole por parámetro el arreglo ListaProdProductos.
+-Luego hay varios eventos configurados, los explicare en comentarios en cada parte respectivamente/
 */
 
 document.addEventListener("DOMContentLoaded", function (e) {
     let id = localStorage.getItem("catID");
     getJSONData(PRODUCTS_URL + id + ".json").then(function (resultObj) {
         if (resultObj.status === "ok") {
-            ProductosCategoria = resultObj.data;
-            MostrarCategoria(ProductosCategoria);
+            ListadeProductos = resultObj.data;
+            document.getElementById("parrafo").innerHTML += ListadeProductos.catName.toLowerCase() + '.';
+            MostrarListaProductos(ListadeProductos);       
         }
     });
-    document.getElementById("sortByMoneyasc").addEventListener("click", function () {
-        OrdenaYMuestra(ORDER_ASC_BY_COST);
+
+    //Este evento configura lo que pasa cuando se apreta en el boton para ordenar los productos 
+    // de forma creciente desde el precio mas bajo al mas alto
+    document.getElementById("OrdenPrecioAsc").addEventListener("click", function () {
+        OrdenaProductos(ORDENAR_ASC_POR_COSTO,ListadeProductos);
+        MostrarListaProductos(ListadeProductos);
     });
-
-    document.getElementById("sortByMoneydesc").addEventListener("click", function () {
-        OrdenaYMuestra(ORDER_DESC_BY_COST);
+    //Este evento configura lo que pasa cuando se apreta en el boton para ordenar los productos 
+    // de forma decreciente desde el precio mas alto al mas bajo
+    document.getElementById("OrdenPrecioDesc").addEventListener("click", function () {
+        OrdenaProductos(ORDENAR_DESC_POR_COSTO,ListadeProductos);
+        MostrarListaProductos(ListadeProductos);
     });
-
-    document.getElementById("Rel.").addEventListener("click", function () {
-        OrdenaYMuestra(ORDER_BY_PROD_SOLDCOUNT);
-
+    //Este evento configura lo que pasa cuando se apreta en el boton para ordenar los productos 
+    // segun su relevancia,  desde el precio mas alto al mas bajo
+    document.getElementById("Relevancia").addEventListener("click", function () {
+        OrdenaProductos(ORDER_POR_CANT_VEND,ListadeProductos);
+        MostrarListaProductos(ListadeProductos);
     });
+    //Evento para filtrar en un rango de precios
+    document.getElementById("FiltroRangoPrecios").addEventListener("click", function () {
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+        //de productos por categoría.
+        CostoMinimo = document.getElementById("CostoMinimo").value;
+        CostoMaximo = document.getElementById("CostoMaximo").value;
 
-    document.getElementById("clearRangeFilter").addEventListener("click", function () {
+        if ((CostoMinimo != undefined) && (CostoMinimo != "") && (parseInt(CostoMinimo)) >= 0) {
+            CostoMinimo = parseInt(CostoMinimo);
+        }
+        else {
+            CostoMinimo = undefined;
+        }
+
+        if ((CostoMaximo != undefined) && (CostoMaximo != "") && (parseInt(CostoMaximo)) >= 0) {
+            CostoMaximo = parseInt(CostoMaximo);
+        }
+        else {
+            CostoMaximo = undefined;
+        }
+        MostrarListaProductos(ListadeProductos);
+    });
+    //Evento para limpiar la casilla de filtro en un rango de precios
+    document.getElementById("LimpiarCasilla").addEventListener("click", function () {
         document.getElementById("rangeFilterCostMin").value = "";
         document.getElementById("rangeFilterCostMax").value = "";
 
-        mincost = undefined;
-        maxcost = undefined;
+        CostoMinimo = undefined;
+        CostoMaximo = undefined;
 
-        MostrarCategoria(ProductosCategoria);
+        MostrarListaProductos(ListadeProductos);
     });
-    document.getElementById("rangeFilterCost").addEventListener("click", function () {
-        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
-        //de productos por categoría.
-        mincost = document.getElementById("rangeFilterCostMin").value;
-        maxcost = document.getElementById("rangeFilterCostMax").value;
-
-        if ((mincost != undefined) && (mincost != "") && (parseInt(mincost)) >= 0) {
-            mincost = parseInt(mincost);
-        }
-        else {
-            mincost = undefined;
-        }
-
-        if ((maxcost != undefined) && (maxcost != "") && (parseInt(maxcost)) >= 0) {
-            maxcost = parseInt(maxcost);
-        }
-        else {
-            maxcost = undefined;
-        }
-        MostrarCategoria(ProductosCategoria);
-    });
+    //Evento para la busqueda en tiempo real
     document.getElementById("buscar").addEventListener("input", function () {
         let ProductoBuscado = document.getElementById('buscar').value;
-        console.log(ProductoBuscado);
-        //console.log(ProductosCategoria.products);
         BuscaYMuestra(ProductoBuscado);
     });
 });
--- js/sell.js
+++ js/sell.js
@@ -28,19 +28,6 @@ function updateTotalCosts() {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    let usuario = sessionStorage.getItem("mail");
-    if (usuario == null) {
-        alert("Perdon, necesita logearse.")
-        alert("Redireccionando a la pagina de login.")
-        location.href = "login.html";
-    }else{
-        document.getElementById("usuario").innerHTML=usuario;
-    }
-    /*Seccion para configurar el boton de cerrar sesion de la barra de navegacion */
-    document.getElementById("Cerrar_sesion").addEventListener("click", function () {
-        sessionStorage.clear();
-        location.href = "login.html"
-    })
 
     document.getElementById("productCountInput").addEventListener("change", function () {
         productCount = this.value;
